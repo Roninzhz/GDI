8ac947d1d387f1490274a3a4833930cb59613976
1938917626@qq.com <1938917626@qq.com>
2019-12-09 08:05:27 +0800

    运行代码清理

--- GDI/GDI/CheckCode.aspx.cs
+++ GDI/GDI/CheckCode.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace GDI
 {
