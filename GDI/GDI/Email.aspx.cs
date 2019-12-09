df5dc6abc1ca62e45528d2ceb1c34a150f507977
1938917626@qq.com <1938917626@qq.com>
2019-12-09 09:41:25 +0800

    加入简单邮箱验证

---
+++ GDI/GDI/Email.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GDI
+{
+    public partial class Email : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private string createrandom(int codelengh)
+        {
+            //生成6位数字和大写字母的验证码
+            int rep = 0;
+            string str = string.Empty;
+            long num2 = DateTime.Now.Ticks + rep;
+            rep++;
+            Random random = new Random(((int)(((ulong)num2) & 0xffffffffL)) | ((int)(num2 >> rep)));
+            for (int i = 0; i < codelengh; i++)
+            {
+                char ch;
+                int num = random.Next();
+                if ((num % 2) == 0)
+                {
+                    ch = (char)(0x30 + ((ushort)(num % 10)));
+                }
+                else
+                {
+                    ch = (char)(0x41 + ((ushort)(num % 0x1a)));
+                }
+                str = str + ch.ToString();
+            }
+            return str;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //实例化一个发送邮件类。
+            MailMessage mailMessage = new MailMessage();
+            //发件人邮箱地址，方法重载不同，可以根据需求自行选择。
+            mailMessage.From = new MailAddress("1938917626@qq.com");
+            //收件人邮箱地址。
+            mailMessage.To.Add(new MailAddress(TextBox1.Text));
+            //邮件标题。
+            mailMessage.Subject = "这是你的验证码";
+            string verificationcode = createrandom(6);
+            //邮件内容。
+            mailMessage.Body = "你的验证码是" + verificationcode;
+            //实例化一个SmtpClient类。
+            SmtpClient client = new SmtpClient();
+            //在这里我使用的是qq邮箱，所以是smtp.qq.com，如果你使用的是126邮箱，那么就是smtp.126.com。
+            client.Host = "smtp.qq.com";
+            //使用安全加密连接。
+            client.EnableSsl = true;
+            //不和请求一块发送。
+            client.UseDefaultCredentials = false;
+            //验证发件人身份(发件人的邮箱，邮箱里的生成授权码);
+            client.Credentials = new NetworkCredential("1938917626@qq.com", "cgixiolxtkllfdge");
+            //发送
+            client.Send(mailMessage);
+            Response.Write("发送成功");
+
+        }
+    }
+}
